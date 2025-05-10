b4a6062e5a3adc8359b92e3f61c53d579fe49ae0
Neil Justine A. Tan <168892372+niltanyowyow@users.noreply.github.com>
2025-05-10 15:35:42 +0800

    Update EditProfile

--- PHP/registerUser.php
+++ PHP/registerUser.php
@@ -17,7 +17,7 @@
         if (isset($_GET['error'])) {
             echo '<div class="alert alert-danger">'.htmlspecialchars($_GET['error']).'</div>';
         }
-      
+      ?>
         <form method="POST" action="../PHP/registerUserFunction.php">
          
             <div class="mb-3">
